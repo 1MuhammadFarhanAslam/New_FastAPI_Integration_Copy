d42faa29f2fd9a8dfae7fa6fadca18ba78b36a19
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-20 00:58:59 +0500

    Update ttm.py

--- classes/ttm.py
+++ classes/ttm.py
@@ -102,17 +102,22 @@ class MusicGenerationService(AIModelService):
         # Network querying logic
         if duration == 15:
             self.duration = 755
+            time_out = 300
         elif duration == 30:
             self.duration = 1510
+            time_out = 600
         elif duration == 45:
             self.duration = 2265
+            time_out = 800
         elif duration == 60:
             self.duration = 3020
+            time_out = 1000
 
         responses = self.dendrite.query(
             filtered_axons,
             lib.protocol.MusicGeneration(text_input=prompt, duration=self.duration ),
             deserialize=True,
+            timeout=time_out,
         )
         return responses
     
