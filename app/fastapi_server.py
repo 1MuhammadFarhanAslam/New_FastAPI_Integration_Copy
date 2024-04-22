2e37285b796d1e083be5caefb6f4d0a07e361d3b
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2024-04-22 16:32:15 +0500

    Update fastapi_server.py

--- app/fastapi_server.py
+++ app/fastapi_server.py
@@ -24,12 +24,38 @@ def create_app(secret_key: str):
     app = FastAPI()
 
     #Allow CORS for only the React frontend server
+    # origins = [
+    #     "http://85.239.241.96:3000",  # Your React frontend server's HTTP URL
+    #     "http://api.bittaudio.ai",
+    #     "http://144.91.69.154:8000",
+    #     "http://localhost:3000",
+    #     "http:127.0.0.1:3000",
+    #     "http://89.37.121.214:44107",
+    #     "http://149.11.242.18:14428",
+    #     "http://bittaudio.ai",
+    #     "http://v1.bittaudio.ai",
+    #     "http://v2.bittaudio.ai",
+    # ]
+
+
+    # # Allow CORS only if not handled by Nginx
+    # app.add_middleware(
+    #     CORSMiddleware,
+    #     allow_origins=origins,
+    #     allow_credentials=True,
+    #     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    #     allow_headers=["*"],
+    #     expose_headers=["*"],
+    # )
+
+
+    # Define the list of allowed origins
     origins = [
         "http://85.239.241.96:3000",  # Your React frontend server's HTTP URL
         "http://api.bittaudio.ai",
         "http://144.91.69.154:8000",
         "http://localhost:3000",
-        "http:127.0.0.1:3000",
+        "http://127.0.0.1:3000",
         "http://89.37.121.214:44107",
         "http://149.11.242.18:14428",
         "http://bittaudio.ai",
@@ -37,17 +63,14 @@ def create_app(secret_key: str):
         "http://v2.bittaudio.ai",
     ]
 
-
-    # Allow CORS only if not handled by Nginx
+    # Allow CORS for all origins specified in the list
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
         allow_credentials=True,
-        allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allow_methods=["GET", "POST", "PUT", "DELETE"],
         allow_headers=["*"],
-        expose_headers=["*"],
     )
-
 
     # Include routers
     app.include_router(login.router, prefix="", tags=["Authentication"])
