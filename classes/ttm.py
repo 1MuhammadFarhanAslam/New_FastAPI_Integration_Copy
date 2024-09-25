8d3837f86c175906f57d1adbfa02ed09d63c3d69
sherijigarx <155580193+sherijigarx@users.noreply.github.com>
2024-09-26 00:42:42 +0500

    Update ttm.py

--- classes/ttm.py
+++ classes/ttm.py
@@ -185,6 +185,7 @@ class MusicGenerationService(AIModelService):
         try:
             # Convert the list to a tensor
             speech_tensor = torch.Tensor(music_output)
+            print(f"Speech Tensor__________________________________: {speech_tensor}")
             # Normalize the speech data
             audio_data = speech_tensor / torch.max(torch.abs(speech_tensor))
 
