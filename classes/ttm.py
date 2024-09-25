12da06bf84f9999a3e95e572fb1e5601f9dd0fe8
sherijigarx <155580193+sherijigarx@users.noreply.github.com>
2024-09-26 00:53:59 +0500

    Update ttm.py

--- classes/ttm.py
+++ classes/ttm.py
@@ -58,7 +58,6 @@ class MusicGenerationService(AIModelService):
             try:
                 await self.main_loop_logic(step)
                 step += 1
-                print(f"Step ----------- : {step}")
             except KeyboardInterrupt:
                 print("Keyboard interrupt detected. Exiting MusicGenerationService.")
                 break
